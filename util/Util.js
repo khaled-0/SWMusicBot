1cbf4c1113f10e11a26c88d531e8ec82fa44df8e
Khaled <74397286+khaled-0@users.noreply.github.com>
2021-10-28 12:40:58 +0000

    Util.js fix

--- util/Util.js
+++ util/Util.js
@@ -8,11 +8,11 @@ try {
   config = null;
 }
 
-exports.TOKEN = process.env.['discordToken'];
-exports.SOUNDCLOUD_CLIENT_ID = process.env.['SOUNDCLOUD_CLIENT_ID'];
-exports.PREFIX = (config ? config.PREFIX : process.env.PREFIX) || "_");
+exports.TOKEN = process.env['discordToken'];
+exports.SOUNDCLOUD_CLIENT_ID = process.env['SOUNDCLOUD_CLIENT_ID'];
+exports.PREFIX = (config ? config.PREFIX : "/");
 exports.MAX_PLAYLIST_SIZE = (config ? config.MAX_PLAYLIST_SIZE : 10);
-exports.PRUNING = (config ? config.PRUNING : false));
+exports.PRUNING = (config ? config.PRUNING : false);
 exports.STAY_TIME = (config ? config.STAY_TIME : 30);
 exports.DEFAULT_VOLUME = (config ? config.DEFAULT_VOLUME : 100);
 exports.LOCALE = (config ? config.LOCALE : "en");
