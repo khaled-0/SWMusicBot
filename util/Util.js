da2ef69ee66274d34909d725f329cf402809a679
Khaled <74397286+khaled-0@users.noreply.github.com>
2021-10-28 10:58:05 +0000

    Removed Unnecessary Files

--- util/Util.js
+++ util/Util.js
@@ -9,10 +9,10 @@ try {
 }
 
 exports.TOKEN = process.env.['discordToken'];
-exports.SOUNDCLOUD_CLIENT_ID = config ? config.SOUNDCLOUD_CLIENT_ID : process.env.SOUNDCLOUD_CLIENT_ID;
-exports.PREFIX = (config ? config.PREFIX : process.env.PREFIX) || "/";
-exports.MAX_PLAYLIST_SIZE = (config ? config.MAX_PLAYLIST_SIZE : parseInt(process.env.MAX_PLAYLIST_SIZE)) || 10;
-exports.PRUNING = (config ? config.PRUNING : (process.env.PRUNING === 'true' ? true : false));
-exports.STAY_TIME = (config ? config.STAY_TIME : parseInt(process.env.STAY_TIME)) || 30;
-exports.DEFAULT_VOLUME = (config ? config.DEFAULT_VOLUME : parseInt(process.env.DEFAULT_VOLUME)) || 100;
-exports.LOCALE = (config ? config.LOCALE : process.env.LOCALE) || "en";
+exports.SOUNDCLOUD_CLIENT_ID = process.env.['SOUNDCLOUD_CLIENT_ID'];
+exports.PREFIX = (config ? config.PREFIX : process.env.PREFIX) || "_");
+exports.MAX_PLAYLIST_SIZE = (config ? config.MAX_PLAYLIST_SIZE : 10);
+exports.PRUNING = (config ? config.PRUNING : false));
+exports.STAY_TIME = (config ? config.STAY_TIME : 30);
+exports.DEFAULT_VOLUME = (config ? config.DEFAULT_VOLUME : 100);
+exports.LOCALE = (config ? config.LOCALE : "en");
